7c091891ac2c565858b62f26b35d7504df7357f3
djakhongirovumedjon <117729551+djakhongirovumedjon@users.noreply.github.com>
2023-01-12 21:09:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let instagram = document.querySelector('.instagram')
+let telegram = document.querySelector('.telegram')
+let telephone = document.querySelector('.telephone')
+
+instagram.onclick = () =>{
+    window.location.href = "https://www.instagram.com/_istanbul__kids_/"
+}
+telegram.onclick = () =>{
+    window.location.href = "https://t.me/+k38WCoHY9WhlYjE6"
+}
+telephone.onclick = () =>{
+    window.location.href = "tel:+998979033225"
+}
